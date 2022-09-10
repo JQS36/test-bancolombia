6101e837a1cbeb13c7ccdf8af6d948d8c778a94a
Jorge Alberto Quiroz Sierra <jorgeavanzado@gmail.com>
2022-09-10 15:22:03 -0500

    Fix models

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,15 +8,21 @@ use App\Models\products;
 class ProductsController extends Controller
 {
     public function getProducts(){
-        return response()->json(products::all(),200);
+        return response()->json([
+            "State" => 1,
+            "Products" => products::all()
+        ],200);
     }
 
     public function getProduct($identifier){
         $product = products::query()->where("identifier_id", $identifier)->first();
         if(is_null($product)){
-            return response()->json(['Msg'=> 'Product not found'],404); 
+            return response()->json([
+                "State" => 0,
+                'Msg' => 'Product not found'
+            ],404); 
         }
-        return response()->json($product,200);
+        return response()->json([ "State"=>1, "Product"=>$product],200);
     }
 
     public function addProduct(Request $request){
@@ -29,22 +35,33 @@ class ProductsController extends Controller
         } catch (\Throwable $th) {
             return ["Fix"=>$th->getMessage()];
         }
-        return response(["answer"=>"Saved correctly", "product"=>$product], 200);
+        return response([
+            "State" => 1,
+            "answer" => "Saved correctly",
+            "product" => $product], 200);
     }
 
     public function updateProduct(Request $request, $id){
         $product = products::query()->where("identifier_id",$id);
         if(is_null($product)){
-            return response()->json(['Msg'=> 'Register not found'],404); 
+            return response()->json([
+                "State" => 0,
+                'Msg'=> 'Register not found'
+            ],404); 
         }
         $product->update($request->all());
-        return response(["answer"=>"Update successfully", "product"=>$product], 200);
+        return response([
+            "State"=> 1,
+            "answer"=>"Update successfully",
+            "product"=>$product->first()], 200);
     }
-
     
     public function deleteProduct($identifier){
         $product = products::query()->where("identifier_id", $identifier)->delete();
-        return response(["answer"=>"Deleted successfully asin: {$identifier}"], 200);
+        return response([
+            "State"=> 1,
+            "answer"=>"Deleted successfully asin: {$identifier}"
+        ], 200);
     }
 
 }
